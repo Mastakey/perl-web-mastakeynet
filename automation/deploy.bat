44973666075973350f78c9833627f43a6003b372
Kioh Han <kioh.han@gmail.com>
2012-07-29 04:00:35 -0400

    Added deploy script Updated kb link

---
+++ automation/deploy.bat
@@ -0,0 +1,7 @@
+cd ..
+xcopy /Y /S config C:\Inetpub\perl-deployment\stage\mastakey\config
+xcopy /Y /S html C:\Inetpub\perl-deployment\stage\mastakey\html
+xcopy /Y /S tmpl C:\Inetpub\perl-deployment\stage\mastakey\tmpl
+xcopy /Y /S cgi-bin C:\Inetpub\perl-deployment\stage\cgi-bin\mastakey
+cd ..
+cd automation
